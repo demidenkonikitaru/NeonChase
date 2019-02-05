ae5808f308db3dd99b32e0593d5952c0a36daa03
Nikita Demidenko <demidenko@storymode.pro>
2019-02-05 19:29:57 +0300

    Basic platform controls and ball.

---
+++ Assets/Scripts/PlatformConfigs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlatformConfigs", menuName = "Configs/PlatformConfigs")]
+public class PlatformConfigs : ScriptableObject
+{
+
+    public float speed;
+    public float xMoveRestriction;
+
+}
---
+++ Assets/Scripts/PlatformControls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Summary: Contains controls for main platform controolled by player. */
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlatformControls : MonoBehaviour
+{
+
+    [SerializeField] private PlatformConfigs platformConfigs;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    private Vector2 moveInput;
+
+    private Vector2 moveVelocity;
+
+    void Start()
+    {
+        if (!rb)
+            rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+        moveVelocity = moveInput.normalized * platformConfigs.speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.position = new Vector2
+        (
+            Mathf.Clamp(rb.position.x,
+            -4f, 4f),
+           rb.position.y
+        );
+
+        //if(rb.position.x > - platformConfigs.xMoveRestriction && rb.position.x < platformConfigs.xMoveRestriction)
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+        
+    }
+}
